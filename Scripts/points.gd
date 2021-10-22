fc7c509f5f74f883e45bf7dd4666c27df7a8d314
Sntx626 <langer.leonard@protonmail.com>
2021-10-22 22:26:48 +0200

    Added Scripts folder

---
+++ Scripts/points.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var currency_t0 = 0
+var currency_t1 = 0
+var currency_t2 = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass

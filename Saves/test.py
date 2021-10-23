fcb284b5c74ba5976da96bc78bd8daec4fe26b9e
Sntx626 <langer.leonard@gmail.com>
2021-10-23 23:26:45 +0200

    changed plotting

--- Saves/test.py
+++ Saves/test.py
@@ -10,11 +10,11 @@ y = []
 
 temp = 0
 for i in data:
-    temp += i[1]*0.000001
-    x.append(temp)
+    #temp += i[1]*0.000001
+    #x.append(temp)
     y.append(i[0]*1000)
 
-xpoints = np.array(x)
+xpoints = np.array(range(len(data)))
 ypoints = np.array(y)
 
 plt.plot(xpoints, ypoints)

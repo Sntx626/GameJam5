ac5ed93e083a62007f9f7d7f679c890fe5e05dfa
Sntx626 <langer.leonard@gmail.com>
2021-10-23 23:18:29 +0200

    added debugging IV

--- Saves/test.py
+++ Saves/test.py
@@ -8,10 +8,13 @@ with open("clicks.dat") as f:
 x = []
 y = []
 
+temp = 0
 for i in data:
+    temp += i[1]*0.000001
+    x.append(temp)
     y.append(i[0])
 
-xpoints = np.array(range(len(data)))
+xpoints = np.array(x)
 ypoints = np.array(y)
 
 plt.plot(xpoints, ypoints)

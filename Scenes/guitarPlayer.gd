656e5136f06848f8fc75fc368e228d5c4ba065ca
s7ud3nt <marcel.oppermann@uni-oldenburg.de>
2021-10-24 16:34:07 +0200

    background animation (tier 0)

---
+++ Scenes/guitarPlayer.gd
@@ -0,0 +1,30 @@
+extends AnimatedSprite
+
+var toCampfire = false
+var t = Timer.new()
+
+
+func goToCampfire():
+	toCampfire = true
+	
+	t.set_one_shot(true)
+	self.add_child(t)
+	play("standUp")
+	t.start(1.25)
+	yield(t, "timeout")
+	t.set_wait_time(0.25)
+	play("walkingSide")
+	for i in range(195):
+		translate(Vector2(-1.5, 0))
+		t.start()
+		yield(t, "timeout")
+	play("walkingBack")
+	for i in range(15):
+		translate(Vector2(0, -1.5))
+		t.start()
+		yield(t, "timeout")
+	flip_h = false
+	play("sitDown")
+	t.start(1.25)
+	yield(t, "timeout")
+	play("playing")
